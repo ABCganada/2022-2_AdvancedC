f0d5f10fe9b3396b3d148e19c4720512916d3276
ABCganada <shinmg9712@gmail.com>
2022-10-21 22:13:34 +0900

    commit

--- 구조체2-2.c
+++ 구조체2-2.c
@@ -2,66 +2,70 @@
 #include <string.h> //string 라이브러리
 #include <stdlib.h> //stdlib 라이브러리
 
-typedef struct student{ //student 구조체
-    char name[21];  //공백없이 최대 길이 20
-    int kor, eng, math;    //국영수 성적
-    double avg;      //세 과목의 평균 점수
-}st;
+// 1차 인증시험 대비 문제 풀이입니다. 
 
-void read_data(st *ptr, int n); //read_data 선언
-void sort_score(st *ptr, int n);    //sort_score 선언
-st *select_out(st *ptr1, st *ptr2, int n);  //select_out 선언
+typedef struct student{
+    char name[21];
+    int kor, eng, mat;
+    double avg;
+}student;
+
+void read_data(student *pst, int N);
+void sort_score(student *pst, int N);
+student *select_out(student *pst1, student *pst2, int N);
 
 int main()
-{  
-    st s1[49], s2[49], *res;    //구조체 변수
-    int n, i;   //int형 변수
-    scanf("%d", &n);    //n입력
+{
+    student std1[49], std2[49], *Output, *p;
+    int n;
+    scanf("%d", &n);
     getchar();
 
-    read_data(s1, n);   //read_data 함수 호출
-    read_data(s2, n);
+    read_data(std1, n);
+    read_data(std2, n);
 
-    sort_score(s1, n);  //sort_score 함수 호출
-    sort_score(s2, n);
+    sort_score(std1, n);
+    sort_score(std2, n);
 
-    res = select_out(s1, s2, n); //res에 리턴된 주소 저장
+    Output = select_out(std1, std2, n);
 
-    for(i=0; i<n; i++, res++){  //출력
-        printf("%s %d %d %d %.1lf\n", res->name, res->kor, res->eng, res->math, res->avg);
+    for(p=Output; p<Output+n; p++){
+        printf("%s %d %d %d %.1lf\n", p->name, p->kor, p->eng, p->mat, p->avg);
     }
-
     
+
+
     return 0;
 }
-void read_data(st *ptr, int n){ //read_data 함수
-    st *p;  //반복문을 위한 구조체 포인터 변수
-    for(p=ptr; p<ptr+n; p++){   //이름, 국영수, 평균
-        scanf("%s", p->name);   //이름 입력
-        scanf("%d %d %d", &p->kor, &p->eng, &p->math);  //국영수 입력
+void read_data(student *pst, int N){
+    student *p;
+    for(p=pst; p<pst+N; p++){
+        scanf("%s", p->name);
+        scanf("%d %d %d", &p->kor, &p->eng, &p->mat);
         getchar();
-        p->avg = (p->kor + p->eng + p->math) / 3.0; //평균값 저장
+        p->avg = (p->kor + p->eng + p->mat) / 3.0;
     }
 }
-void sort_score(st *ptr, int n){    //sort_score 함수
-    st *p, tmp; //반복문을 위한 변수, swap을 위한 구조체 tmp
-    int i;  //반복문을 위한 int형 변수
-    for(i=0; i<n-1; i++){   //버블 정렬
-        for(p=ptr; p<ptr+n-1; p++){
-            if(p->avg < (p+1)->avg){    //평균이 다름
-                tmp = *p;   //swap
+void sort_score(student *pst, int N){
+    student *p, tmp;
+    int i;
+
+    for(i=0; i<N-1; i++){
+        for(p=pst; p<pst+N-1; p++){
+            if(p->avg < (p+1)->avg){
+                tmp = *p;
                 *p = *(p+1);
                 *(p+1) = tmp;
             }
-            else if (p->avg == (p+1)->avg){ //평균이 같음
-                if(p->kor < (p+1)->kor){    //국어 점수 다름
-                    tmp = *p;   //swap
+            else if(p->avg == (p+1)->avg){
+                if(p->kor < (p+1)-> kor){
+                    tmp = *p;
                     *p = *(p+1);
                     *(p+1) = tmp;
                 }
-                else if(p->kor == (p+1)->kor){  //평균 같음, 국어 점수 같음
-                    if(p->eng < (p+1)->eng){    //영어 점수 다름
-                        tmp = *p;   //swap
+                else if(p->kor == (p+1)->kor){
+                    if(p->eng < (p+1)->eng){
+                        tmp = *p;
                         *p = *(p+1);
                         *(p+1) = tmp;
                     }
@@ -70,10 +74,10 @@ void sort_score(st *ptr, int n){    //sort_score 함수
         }
     }
 }
-st *select_out(st *ptr1, st *ptr2, int n){  //select_out 함수
-    double n1 = (ptr1 + n/2)->avg;  //첫 배열 중간값
-    double n2 = (ptr2 + n/2)->avg;  //두번째 배열 중간값
-
-    if(n1 >= n2) return ptr1;   //첫 배열 중간값이 크다면
-    else    return ptr2;    //두 번째 배열 중간값이 크다면
+student *select_out(student *pst1, student *pst2, int N){
+    if((pst1+(N/2))->avg > (pst2+(N/2))->avg){
+        return pst1;
+    }
+    else    return pst2;
 }
+// 1차 인증시험 대비 문제풀이입니다.
